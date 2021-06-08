9e37c7c78023167e391e332b8a2c5bcdaf69d906
Natalia Sharapova <emuradyants2005@gmail.com>
2021-06-08 22:10:35 +0300

    Added new colors (rainbow). After purple color the screen returns to white color and repeats the cycle again. Removed the black color.

--- The light/ViewController.swift
+++ The light/ViewController.swift
@@ -7,23 +7,48 @@
 
 import UIKit
 
+var i = 0
+
 class ViewController: UIViewController {
-    
-    var isLightOn = true
     
     override var prefersStatusBarHidden: Bool {
         return true
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        }
+        
+    view.backgroundColor = .white
+    }
     
     @IBAction func buttonPushed() {
-        isLightOn.toggle()
-        view.backgroundColor = isLightOn ? .white : .black
-    }
         
+    view.backgroundColor = .white
+        
+    i += 1
+            
+        switch i {
+        
+        case 1:
+    view.backgroundColor = .red
+        case 2:
+    view.backgroundColor = .orange
+        case 3:
+    view.backgroundColor = .yellow
+        case 4:
+    view.backgroundColor = .green
+        case 5:
+    view.backgroundColor = .blue
+        case 6:
+    view.backgroundColor = .purple
+
+        default:
+    i = 0
+            
     }
+    
+    }
+
+    }
+
 
 
